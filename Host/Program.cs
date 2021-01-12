d6177ec2d6155175f64ceabf53ea5306cd85743a
Krystian <k.szot@nextrope.com>
2021-01-12 18:29:53 +0100

    Customer database good

---
+++ Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApi;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serwer Biblioteki");
+            ServiceHost host = new ServiceHost(typeof(LibraryService));
+
+            host.Open();
+            Console.WriteLine("Serwer działa. Aby przerwać nacisnij dowolny przycisk");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LibraryApi/LibraryService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LibraryApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class LibraryService : ILibraryService
+    {
+        public string AddClientToDatabase(Klienci client)
+        {
+            var context = new libraryEntities();
+
+            context.Kliencis.Add(client);
+            context.SaveChanges();
+
+            return "Dodano Klienta";
+        }
+
+        public void ModifyClient(Klienci client)
+        {
+            var context = new libraryEntities();
+            var person = context.Kliencis.Find(client.IDKlienta);
+            person.Imie = client.Imie;
+            person.Nazwisko = client.Nazwisko;
+            person.Plec = client.Plec;
+            person.Wiek = client.Wiek;
+            person.Adres = client.Adres;
+            person.Telefon = client.Telefon;
+            context.SaveChanges();
+        }
+        public void DeleteClientAfterID(int index)
+        {
+            var context = new libraryEntities();
+            var person = context.Kliencis.Where(e => e.IDKlienta == index).FirstOrDefault();
+            context.Kliencis.Remove(person);
+            context.SaveChanges();
+        }
+        public Klienci GetLastClient()
+        {
+            var context = new libraryEntities();
+            var klient = context.Kliencis.OrderByDescending(el => el.IDKlienta).FirstOrDefault();
+            return klient;
+        }
+
+        public Klienci FetchClientAfterID(int index)
+        {
+            var context = new libraryEntities();
+            var klient = context.Kliencis.Where(e => e.IDKlienta == index).FirstOrDefault();
+            return klient;
+        }
+        public IEnumerable<Klienci> GetAllClient()
+        {
+            var context = new libraryEntities();
+            return context.Kliencis;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
